e0081901820f10ae624fe32909cec10b04d2947b
Gabriel Vacari <gabriel_vacari@outlook.com>
2020-03-05 21:01:30 -0300

    persisting data

--- app/src/main/java/com/example/portal/MainActivity.kt
+++ app/src/main/java/com/example/portal/MainActivity.kt
@@ -13,35 +13,43 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var sh = getSharedPreferences("valores", Context.MODE_PRIVATE)
+        var sh = getSharedPreferences("valores", Context.MODE_PRIVATE )
 
-
+        fun persistirDados(marketPlace : String){
+            sh.edit()
+                .putInt(marketPlace, sh.getInt(marketPlace,0) +1).apply()
+        }
 
         mercadoLivre.setOnClickListener {
+            persistirDados("mercadoLivre")
             var intent = Intent(this, Portal::class.java)
             intent.putExtra("url", "https://www.mercadolivre.com.br/")
             startActivity(intent)
         }
 
         amazon.setOnClickListener{
+            persistirDados("amazon")
             var intent = Intent(this, Portal::class.java)
             intent.putExtra("url", "https://www.amazon.com.br/")
             startActivity(intent)
         }
 
         alibaba.setOnClickListener{
+            persistirDados("alibaba")
             var intent = Intent(this, Portal::class.java)
             intent.putExtra("url", "https://www.alibaba.com/")
             startActivity(intent)
         }
 
         ebay.setOnClickListener{
+            persistirDados("ebay")
             var intent = Intent(this, Portal::class.java)
             intent.putExtra("url", "https://www.ebay.com/")
             startActivity(intent)
         }
 
         submarino.setOnClickListener{
+            persistirDados("submarino")
             var intent = Intent(this, Portal::class.java)
             intent.putExtra("url", "https://www.submarino.com.br/")
             startActivity(intent)
--- app/src/main/java/com/example/portal/Sobre.kt
+++ app/src/main/java/com/example/portal/Sobre.kt
@@ -1,7 +1,9 @@
 package com.example.portal
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sobre.*
 
 class Sobre : AppCompatActivity() {
 
@@ -9,10 +11,18 @@ class Sobre : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sobre)
 
-        // Pega contador de cada portal
+        var sh = getSharedPreferences("valores", Context.MODE_PRIVATE)
 
-//        var pegaValorDoPoral
+        txt.text = "Mercado Livre: ${sh.getInt("mercadoLivre", 0)} \n" +
+                "Amazon: ${sh.getInt("amazon", 0)} \n" +
+                "Alibaba: ${sh.getInt("alibaba", 0)} \n" +
+                "Ebay: ${sh.getInt("ebay", 0)} \n" +
+                "Submarino: ${sh.getInt("submarino", 0)} \n"
 
+        limpa.setOnClickListener{
+            sh.edit().clear().apply()
+            recreate()
+        }
 
     }
 }
